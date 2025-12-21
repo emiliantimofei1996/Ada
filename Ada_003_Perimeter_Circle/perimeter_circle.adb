4a22c8f663cf1016a23e0c1acfbce306c433d6a4
emiliantimofei1996 <emilian.timofei@hey.com>
2025-12-21 18:48:00 +0100

    cleaned and commented code

--- Ada_003_Perimeter_Circle/perimeter_circle.adb
+++ Ada_003_Perimeter_Circle/perimeter_circle.adb
@@ -2,8 +2,8 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Perimeter_Circle is
    -- C = 2(pg)r
-   PI : constant := 3.14159;
-   r : Float := 5.0;
+   PI : constant := 3.14159; -- by adding more number the calculation can be more precise! 
+   r : Float := 5.0; -- [ <- this ] change ] ? the perimeter changes 
    C : constant Float := 2.0*PI*r; 
    -- now cook
 begin
@@ -13,20 +13,3 @@ begin
    Put_Line ("The perimeter with the radius r is: " & Float'Image (C));
    
 end Perimeter_Circle;
-
---  with Ada.Text_IO; use Ada.Text_IO;
-
---  procedure Perimeter_Circle is
---     -- C = 2πr
---     PI : constant Float := 3.14159;
---     r : Float := 5.0;
---     C : Float;  -- Variabile normale, NON constant
---  begin
---     -- Calcola il perimetro
---     C := 2.0 * PI * r;
-
---     -- Mostra i risultati
---     Put_Line ("The radius that you choose is: " & Float'Image(r));
---     Put_Line ("The perimeter with the radius r is: " & Float'Image(C));
-   
---  end Perimeter_Circle;
